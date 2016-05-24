dac15d63ae608ad11a6b3d23f45e4acffddb520b
Alexander Michaud <alexandercmzt@gmail.com>
2016-05-24 17:33:20 -0400

    new flags

--- Learner.py
+++ Learner.py
@@ -1,4 +1,10 @@
-#INPUT: list of text files as arguments to use for training
+######################################################################################################################################################################
+#   Written for Computer Vision team on McGill Robotics by Alexander Chatron-Michaud
+#   Usage: 
+#          - First command line argument is flag 1. If cross validation and test sets should be created, use the flag -test. Otherwise use the "full" flag -full
+#          - If .pkl files exist in the DATA folder, they will be loaded upon execution
+#
+######################################################################################################################################################################
 
 import sys, string, pickle, os, time
 import numpy as np
@@ -28,8 +34,13 @@ def addToX(textfile): #Opens "textfile" and adds line-by-line training vectors t
                 f.close()
 
 def setup_fresh(): #takes system arguments which are text files which contain all the examples of that category
-    for index in range(1,len(sys.argv)):
-            addToX(sys.argv[index])
+    if len(sys.argv) < 3 :
+        print "Please enter one or more data files as command line arguments."
+        exit()
+
+    for index in range(2,len(sys.argv)):
+        addToX(sys.argv[index])
+        print str(sys.argv[index]) + "done."
     
     #Use 60% of X as training, 20% as cross validation, and 20% for testing performance
     m = len(X)
@@ -40,24 +51,35 @@ def setup_fresh(): #takes system arguments which are text files which contain al
     X_test = X[test_index:m+1]
 
     #Give zero mean and unit variance
-    scaler = preprocessing.StandardScaler().fit(X_train)
-    X_train = scaler.transform(X_train)
-    X_cv = scaler.transform(X_cv)
-    X_test = scaler.transform(X_test)
-    #joblib.dump(scaler, 'DATA/scaler.pkl')
+    scaler_full = preprocessing.StandardScaler().fit(X)
+    X = scaler_full.transform(X)
+    scaler_test = preprocessing.StandardScaler().fit(X_train)
+    X_train = scaler_test.transform(X_train)
+    X_cv = scaler_test.transform(X_cv)
+    X_test = scaler_test.transform(X_test)
+    joblib.dump(scaler, 'DATA/scaler_test.pkl')
     joblib.dump(X_train, 'DATA/X_train.pkl')
     joblib.dump(X_cv, 'DATA/X_cv.pkl')
     joblib.dump(X_test, 'DATA/X_test.pkl')
 
 def setup_load():
-	X_train = joblib.load('DATA/X_train.pkl')
+    X = joblib.load('DATA/X.pkl')
+    X_train = joblib.load('DATA/X_train.pkl')
     X_cv = joblib.load('DATA/X_cv.pkl')
     X_test = joblib.load('DATA/X_test.pkl')
 
 
 def main():
+    if sys.argv[1] == "-full":
+        full = True
+    elif sys.argv[1] == "-test":
+        full = False
+    else:
+        print "Please use flag -full or -test when running the program"
+        exit()
+
     #If existing models have been parsed, load them, otherwise we parse them fresh
-    if os.path.isfile('DATA/X_train.pkl') and os.path.isfile('DATA/X_cv.pkl') and os.path.isfile('DATA/X_test.pkl'):
+    if os.path.isfile('DATA/X_train.pkl') and os.path.isfile('DATA/X_cv.pkl') and os.path.isfile('DATA/X_test.pkl') and s.path.isfile('DATA/X.pkl'):
         print "Pre-existing .pkl files found, loading from data..."
         setup_load()
     else:
@@ -67,10 +89,14 @@ def main():
 	print "Setup done. Feedforward/Backprop initializing... (This may take some time)"
 
 	init_time = time.time()
-    NN_model = MLPClassifier(algorithm='l-bfgs', alpha=1e-5, hidden_layer_sizes=(550,), random_state=1) #550 is a rough estimate for an appropriate number of hidden units based on Andrew Ng
+    NN_model = MLPClassifier(algorithm='l-bfgs', alpha=1e-5, hidden_layer_sizes=(550,), random_state=1) #550 is a rough estimate for an appropriate number of hidden units
+    if full:
+        NN_model.fit(X,y)
+    else:
+        NN_model.fit(X_train,y)
     print "Feedforward/Backprop done. Training took " + str(time.time() - init_time) + " seconds. Saving model..."
 
-	joblib.dump(NN_model, 'DATA/NN_model.pkl')
+    joblib.dump(NN_model, 'DATA/NN_model.pkl')
     print "Model save successful"
 
 main()
