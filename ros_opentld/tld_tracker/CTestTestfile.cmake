b11eade6d39d44077c6844eba47cfc71c77b5242
Alex Smith <alexsmith1@me.com>
2016-07-04 21:22:34 -0400

    proper reversion to the previous code

--- ros_opentld/tld_tracker/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alexsmith/coding/auv/catkin_ws/src/ros_opentld/tld_tracker
-# Build directory: /home/alexsmith/coding/auv/catkin_ws/build/ros_opentld/tld_tracker
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
