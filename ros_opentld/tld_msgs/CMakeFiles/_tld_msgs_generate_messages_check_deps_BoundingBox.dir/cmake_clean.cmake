0da027b7cadab43104c885a5cb924b752ee6ae48
Alex Smith <alexsmith1@me.com>
2016-07-04 20:15:52 -0400

    Revert "latest code from last work session"
    This reverts commit 33af5243793c3c448f8aa64f77e14c1b7f1fa4ca.

--- ros_opentld/tld_msgs/CMakeFiles/_tld_msgs_generate_messages_check_deps_BoundingBox.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/_tld_msgs_generate_messages_check_deps_BoundingBox"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/_tld_msgs_generate_messages_check_deps_BoundingBox.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- ros_opentld/tld_msgs/catkin_generated/tld_msgs-msg-extras.cmake.installspace.in
+++
@@ -1,2 +0,0 @@
-set(tld_msgs_MESSAGE_FILES "msg/BoundingBox.msg;msg/Target.msg")
-set(tld_msgs_SERVICE_FILES "")
